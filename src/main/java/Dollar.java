43c6d7b49f2e58c7817f1adc3687a69aade5c849
Michele delle Noci <michele.dellenoci@gmail.com>
2020-10-12 22:55:10 +0200

    + Add Franc + Add superclass Money

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,20 +1,11 @@
-import java.util.Objects;
-
-public class Dollar {
-
-    public int amount;
+public class Dollar extends Money {
 
     Dollar(int amount) {
         this.amount = amount;
     }
 
-    Dollar times(int multiplier) {
+    Money times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
 
-    @Override
-    public boolean equals(Object object) {
-        Dollar dollar = (Dollar) object;
-        return amount == dollar.amount;
-    }
 }
